fde4c54223851d6fd8a2d7af512a433cd38b565d
Gene Myers <genemyers@users.noreply.github.com>
2018-10-18 09:21:58 +0100

    Removed emacs, tidied
    - still needs an 'n' vs. 'nvm' strategy

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # Simple setup.sh for configuring Ubuntu 14.04 LTS EC2 instance
 # for headless setup. 
+#
+# This should be in its own Heroku option setup.sh
+#
+# Install Heroku toolbelt
+# https://toolbelt.heroku.com/debian
+# wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+sudo apt-get install -y curl
 
 ##### WAIT- SHOULD NOT NEED N and NVM !!!!!!!!!!
 
-# Install nvm: node-version manager
-# https://github.com/creationix/nvm
-#sudo apt-get install -y git
-sudo apt-get install -y curl
-#curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+# Install nvm: node-version manager 0.10.0
 wget https://raw.githubusercontent.com/creationix/nvm/v0.10.0/install.sh 
 sudo sh install.sh
 
@@ -17,36 +21,22 @@ source $HOME/.nvm/nvm.sh
 nvm install v0.10.29
 nvm use v0.10.29
 
-# Install jshint to allow checking of JS code within emacs
-# http://jshint.com/
-#npm install -g jshint
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
 
-# Install emacs24
-# https://launchpad.net/~cassou/+archive/emacs
-#sudo add-apt-repository -y ppa:cassou/emacs
-#sudo apt-get -qq update
-#sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
-
-# Install Heroku toolbelt
-# https://toolbelt.heroku.com/debian
-# wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # git pull and install dotfiles as well
 cd $HOME
 if [ -d ./dotfiles/ ]; then
     mv dotfiles dotfiles.old
 fi
-#if [ -d .emacs.d/ ]; then
-#   mv .emacs.d .emacs.d~
-fi
+
 git clone https://github.com/genemyers/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
-#ln -sf dotfiles/.emacs.d .
+
 
