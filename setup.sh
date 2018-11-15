73d49a07c9e2c3f433701124798945f1d894158f
Giles Pepper <giles@Bens-MacBook-Pro.local>
2018-11-15 12:29:54 +0000

    FIX: Move common provisioning to stanford script

--- setup.sh
+++ setup.sh
@@ -8,24 +8,19 @@
 # https://toolbelt.heroku.com/debian
 # wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
-sudo apt-get install -y curl
+#sudo apt-get install -y curl
 
 ##### WAIT- SHOULD NOT NEED N and NVM !!!!!!!!!!
 
-# Install nvm: node-version manager 0.10.0
-wget https://raw.githubusercontent.com/creationix/nvm/v0.10.0/install.sh 
-sudo sh install.sh
-
+#The NVM setup is now done in buildscripts/stanford_dev_env.sh
 # Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.29
-nvm use v0.10.29
-
+#source $HOME/.nvm/nvm.sh
+#nvm install v0.10.29
+#nvm use v0.10.29
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
-sudo apt-get install -y rlwrap
-
+#sudo apt-get install -y rlwrap
 
 # git pull and install dotfiles as well
 cd $HOME
@@ -38,5 +33,3 @@ ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
-
-
